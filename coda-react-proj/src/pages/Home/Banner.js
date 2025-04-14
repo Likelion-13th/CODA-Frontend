b51bc03db2a25bcba12dccc994fcd102b8060936
selnem <tjdghks0130@naver.com>
2025-04-15 04:38:47 +0900

    Fix: function name SNAKE_CASE error

--- coda-react-proj/src/pages/Home/Banner.js
+++ coda-react-proj/src/pages/Home/Banner.js
@@ -6,7 +6,7 @@ import "slick-carousel/slick/slick-theme.css"
 import "../../styles/Banner_custom.css"
 
 
-const Custom_Arrow = ({onClick, direction})=>{
+const CustomArrow = ({onClick, direction})=>{
     const isPrev = direction ==="prev";
     const arrowStyle={
         width: "fit-content",
@@ -46,8 +46,8 @@ const Banner = () => {
         autoplay:true,
         autoplaySpeed: 3000,
         arrows: true,
-        prevArrow: <Custom_Arrow direction="prev" />,
-        nextArrow: <Custom_Arrow direction="next" />,
+        prevArrow: <CustomArrow direction="prev" />,
+        nextArrow: <CustomArrow direction="next" />,
     };
     const images=[
         `${process.env.PUBLIC_URL}/img/banner_background_1.jpg`,
